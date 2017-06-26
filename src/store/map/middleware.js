d984f7c671cd94fce3e7b84c87067a02a0d88c00
Johan Gustafsson <johan.gustafsson@solidio.se>
2017-06-26 09:43:26 +0200

    Middleware buffering (#45)
    * First version of middleware buffering
    
    * Refactor
    
    * Clean up
    
    * Refactor
    
    * Refactor
    
    * Made firebase streams instead of subject
    
    * Minor cleanup
    
    * Add child_added and child_removed
    
    * Fix child_removed

--- src/store/map/middleware.js
+++ src/store/map/middleware.js
@@ -1,5 +1,7 @@
 import { updateEntities, removeEntity } from "store/entities/actions";
 import { locateSuccess } from "store/locations/actions";
+import { mergeDeepLeft, pipe, prop, reduce } from "ramda";
+import { Observable } from "rxjs/Rx";
 
 export default function createMiddleware(firebase) {
   return ({ dispatch }) => {
@@ -8,38 +10,50 @@ export default function createMiddleware(firebase) {
       center: [11.9, 57.69],
       radius: 5
     });
+
     const eventsRef = firebase.database().ref("events");
 
-    // Action creators
-    const updateEvents = snap =>
-      dispatch(updateEntities("events", { [snap.key]: snap.val() }));
-    const updateLocations = loc => dispatch(updateEntities("locations", loc));
-
-    /*
-    Set up listeners to update state on changes in locations or events
-    */
-    geoquery.on("key_entered", (key, loc) => {
-      updateLocations({ [key]: loc });
-
-      // Fetch event data and subscribe to changes
-      eventsRef.child(key).on("value", updateEvents);
+    eventsRef.on("child_removed", snap => {
+      dispatch(removeEntity("events", snap.key));
+      dispatch(removeEntity("locations", snap.key));
     });
-    // Location moved
-    geoquery.on("key_moved", (key, loc) => updateLocations({ [key]: loc }));
 
-    // Location deleted
+    const keyEntered$ = Observable.create(observer =>
+      geoquery.on("key_entered", (key, loc) => {
+        const emit = snap =>
+          observer.next({
+            events: { [key]: snap.val() },
+            locations: { [key]: loc }
+          });
+
+        eventsRef.child(key).on("value", emit);
+      })
+    );
+
+    const keyMoved$ = Observable.create(observer =>
+      geoquery.on("key_moved", (key, loc) =>
+        observer.next({ locations: { [key]: loc } })
+      )
+    );
+
     geoquery.on("key_exited", (key, loc) => {
-      // Remove from state
       dispatch(removeEntity("locations", key));
       dispatch(removeEntity("events", key));
 
       // Unsubscribe
-      eventsRef
-        .child(key)
-        .off("value", snap =>
-          updateEntities("events", { [snap.key]: snap.val() })
-        );
+      eventsRef.child(key).off();
     });
+
+    const dispatchBufferedEvents = pipe(
+      reduce(mergeDeepLeft, {}),
+      updateEntities,
+      dispatch
+    );
+
+    const firebase$ = Observable.merge(keyEntered$, keyMoved$)
+      .bufferTime(3000)
+      .filter(events => events.length)
+      .subscribe(dispatchBufferedEvents);
 
     return next => action => {
       if (action.type === locateSuccess().type) {
